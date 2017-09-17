1fa1024193a754576ed4eb047e9f4ed036ac2c5f
Divyendu Singh <divyendu.z@gmail.com>
2017-09-17 23:07:57 +0530

    pre re-recording fix

--- backend/app.js
+++ backend/app.js
@@ -1,94 +1,10 @@
 const express = require("express");
 const graphQLHTTP = require("express-graphql");
-const {
-    GraphQLSchema,
-    GraphQLObjectType,
-    GraphQLNonNull,
-    GraphQLString,
-    GraphQLID,
-    GraphQLList
-} = require("graphql");
+const { GraphQLSchema } = require("graphql");
 
-const fakeDatabase = [
-    { id: 1, title: "post one title", content: "post one content" },
-    { id: 2, title: "post two title", content: "post two content" }
-];
-
-const Post = new GraphQLObjectType({
-    name: "Post",
-    description: "Post type definition",
-    fields: {
-        id: {
-            type: new GraphQLNonNull(GraphQLID),
-            description: "ID of the post",
-            resolve: post => post.id
-        },
-        title: {
-            type: GraphQLString,
-            description: "Title of the post",
-            resolve: post => post.title
-        },
-        content: {
-            type: GraphQLString,
-            description: "Content of the post",
-            resolve: post => post.content
-        }
-    }
-});
-
-const Query = new GraphQLObjectType({
-    name: "Query",
-    description: "Query interface for our blog",
-    fields: {
-        post: {
-            type: Post,
-            description: "Query to get a single post",
-            args: {
-                id: { type: new GraphQLNonNull(GraphQLID) }
-            },
-            resolve: (_, args) => {
-                return fakeDatabase.filter(row => row.id == args.id)[0];
-            }
-        },
-        posts: {
-            type: new GraphQLList(Post),
-            description: "Query to get a all posts",
-            args: {},
-            resolve: (_, args) => {
-                return fakeDatabase;
-            }
-        }
-    }
-});
-
-const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    description: "Mutation interface for our blog",
-    fields: {
-        createPost: {
-            type: Post,
-            args: {
-                title: {
-                    type: GraphQLString,
-                    description: "Title of the post"
-                },
-                content: {
-                    type: GraphQLString,
-                    description: "Content of the post"
-                }
-            },
-            resolve: (_, args) => {
-                const post = {
-                    id: fakeDatabase.length + 1,
-                    title: args.title,
-                    content: args.content
-                };
-                fakeDatabase.push(post);
-                return post;
-            }
-        }
-    }
-});
+const Post = require("./src/types/Post");
+const Query = require("./src/types/Query");
+const Mutation = require("./src/types/Mutation");
 
 const schema = new GraphQLSchema({
     query: Query,
